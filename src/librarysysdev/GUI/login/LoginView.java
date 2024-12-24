8200d5bc7a6ffe3d4355e6212e060d4ff45556f0
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2024-12-23 19:13:11 -0600

    changes on interface

--- src/librarysysdev/GUI/login/LoginView.java
+++ src/librarysysdev/GUI/login/LoginView.java
@@ -1,5 +1,5 @@
 
-package librarysysdev.GUI;
+package librarysysdev.GUI.login;
 
 
 public class LoginView extends javax.swing.JFrame {
@@ -24,7 +24,8 @@ public class LoginView extends javax.swing.JFrame {
     userLabel = new javax.swing.JLabel();
     txtUser = new javax.swing.JTextField();
     passwordLabel = new javax.swing.JLabel();
-    txtPassword = new javax.swing.JTextField();
+    txtPassword = new javax.swing.JPasswordField();
+    loginBtn = new javax.swing.JButton();
 
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -36,6 +37,7 @@ public class LoginView extends javax.swing.JFrame {
     userLabel.setText("User");
 
     txtUser.setBackground(new java.awt.Color(241, 191, 152));
+    txtUser.setForeground(new java.awt.Color(0, 0, 0));
 
     passwordLabel.setBackground(new java.awt.Color(241, 191, 152));
     passwordLabel.setFont(new java.awt.Font("MS UI Gothic", 0, 18)); // NOI18N
@@ -43,23 +45,27 @@ public class LoginView extends javax.swing.JFrame {
     passwordLabel.setText("Password");
 
     txtPassword.setBackground(new java.awt.Color(241, 191, 152));
-    txtPassword.addActionListener(new java.awt.event.ActionListener() {
-      public void actionPerformed(java.awt.event.ActionEvent evt) {
-        txtPasswordActionPerformed(evt);
-      }
-    });
+    txtPassword.setForeground(new java.awt.Color(0, 0, 0));
+    txtPassword.setText("jPasswordField1");
+
+    loginBtn.setText("jButton1");
 
     javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
     jPanel1.setLayout(jPanel1Layout);
     jPanel1Layout.setHorizontalGroup(
       jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
       .addGroup(jPanel1Layout.createSequentialGroup()
-        .addGap(279, 279, 279)
         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-          .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
-          .addComponent(passwordLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
-          .addComponent(txtUser, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
-          .addComponent(userLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
+          .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGap(279, 279, 279)
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+              .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
+              .addComponent(passwordLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+              .addComponent(txtUser, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
+              .addComponent(userLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)))
+          .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGap(336, 336, 336)
+            .addComponent(loginBtn)))
         .addContainerGap(464, Short.MAX_VALUE))
     );
     jPanel1Layout.setVerticalGroup(
@@ -73,7 +79,9 @@ public class LoginView extends javax.swing.JFrame {
         .addComponent(passwordLabel)
         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
         .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-        .addContainerGap(320, Short.MAX_VALUE))
+        .addGap(28, 28, 28)
+        .addComponent(loginBtn)
+        .addContainerGap(247, Short.MAX_VALUE))
     );
 
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -87,15 +95,11 @@ public class LoginView extends javax.swing.JFrame {
       .addGroup(layout.createSequentialGroup()
         .addContainerGap()
         .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        .addContainerGap())
+        .addGap(28, 28, 28))
     );
 
     pack();
   }// </editor-fold>//GEN-END:initComponents
-
-  private void txtPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPasswordActionPerformed
-    // TODO add your handling code here:
-  }//GEN-LAST:event_txtPasswordActionPerformed
 
   /**
    * @param args the command line arguments
@@ -134,8 +138,9 @@ public class LoginView extends javax.swing.JFrame {
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JPanel jPanel1;
+  private javax.swing.JButton loginBtn;
   private javax.swing.JLabel passwordLabel;
-  private javax.swing.JTextField txtPassword;
+  private javax.swing.JPasswordField txtPassword;
   private javax.swing.JTextField txtUser;
   private javax.swing.JLabel userLabel;
   // End of variables declaration//GEN-END:variables
