1b1682f5029d1ace07a2bb573844816412e22e9d
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-01-29 00:50:17 -0600

    add validation pipeline

---
+++ src/librarysysdev/logic/NameValidator.java
@@ -0,0 +1,15 @@
+package librarysysdev.logic;
+
+public class NameValidator implements Validator<String> {
+  private static final String NAME_PATTERN = "[a-zA-Z]+( +[a-zA-Z]+)*";
+  
+  
+  public boolean validate(String input) {
+    return input.matches(NAME_PATTERN);
+  }
+  
+  
+  public String getErrorMessage() {
+    return "Invalid name. Please, only letters and space are allowed.";
+  }
+}
