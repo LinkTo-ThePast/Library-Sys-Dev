96d7105ee9da49b348652d57a90d31340ed032fd
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-01-30 22:55:17 -0600

    add validation pipeline

---
+++ src/librarysysdev/logic/ValidationPipeline.java
@@ -0,0 +1,24 @@
+package librarysysdev.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationPipeline<T> {
+  private final List<Validator<T>> validators = new ArrayList<>();
+  private final List<String> errorMessages = new ArrayList<>();
+  
+  public void addValidator(Validator<T> validator) {
+    validators.add(validator);
+  }
+  
+  public boolean validateAll(T input) {
+    errorMessages.clear();
+    for(Validator<T> validator : validators) {
+      if(validator.validate(input)) {
+        errorMessages.add(validator.getErrorMessage());
+        return false;
+      } 
+    }
+    return true;
+  }
+}
