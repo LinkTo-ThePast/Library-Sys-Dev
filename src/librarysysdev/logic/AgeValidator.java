b4e2f5f378bcbe113b2a3c6600464394933eff2a
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-02-07 00:00:53 -0600

    modify interface validator

--- src/librarysysdev/logic/AgeValidator.java
+++ src/librarysysdev/logic/AgeValidator.java
@@ -2,9 +2,7 @@ package librarysysdev.logic;
 
 public class AgeValidator implements Validator<Integer> {
   
-  public boolean validate(Integer input) {
-    if(input >= 12 && input <= 72) {
-    return true
-    }
+  public boolean validate(Integer age) {
+    return age >= 12 && age <= 72;
   }
 }
