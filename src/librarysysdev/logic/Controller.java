561969e529befb0ff7472d605a942102e7822700
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-02-07 00:37:02 -0600

    validation pipeline & controller

--- src/librarysysdev/logic/Controller.java
+++ src/librarysysdev/logic/Controller.java
@@ -4,7 +4,11 @@ import librarysysdev.GUI.registerNewMember.RegisterNewMember;
 
 public class Controller {
 
- 
+  public boolean validateUser(String firstName, String lastName, int age, String email, int phoneNumber) {
+    ValidationPipeline<String> namePipeline = new ValidationPipeline<>();
+    namePipeline.addValidator(new NameValidator());
+  }
+  
   public boolean validateName(String inputUserFirstName, String inputUserLastName) {
     String regexString = "[a-zA-Z]+( +[a-zA-Z]+)*";
     boolean result = false;
