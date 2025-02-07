b697242a133748de236405c7de6f22d1ed307a1d
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-02-07 00:02:52 -0600

    modify interfaces

--- src/librarysysdev/logic/AgeValidator.java
+++ src/librarysysdev/logic/AgeValidator.java
@@ -2,7 +2,13 @@ package librarysysdev.logic;
 
 public class AgeValidator implements Validator<Integer> {
   
+  @Override
   public boolean validate(Integer age) {
+ 
     return age >= 12 && age <= 72;
+  }
+  
+  public String getErrorMessage() {
+    return "Invalid age. Age must be between 12 and 72.";
   }
 }
