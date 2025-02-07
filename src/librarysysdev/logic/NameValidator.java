3aa1148f11b05e17b16fc978395fb35b1587365e
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-02-07 00:08:40 -0600

    validator interface

--- src/librarysysdev/logic/NameValidator.java
+++ src/librarysysdev/logic/NameValidator.java
@@ -1,16 +1,14 @@
 package librarysysdev.logic;
 
 public class NameValidator implements Validator<String> {
+  private final String NAME_PATTERN = "[a-zA-Z]+( +[a-zA-Z]+)*";
   
-  private static final String NAME_PATTERN = "[a-zA-Z]+( +[a-zA-Z]+)*";
-  
-  
-  public boolean validate(String input) {
-    return input.matches(NAME_PATTERN);
+  @Override
+  public boolean validate(String name) {
+    if(name.matches(NAME_PATTERN)){
+      return true;
+    } else return false;
   }
   
-  
-  public String getErrorMessage() {
-    return "Invalid name. Please, only letters and space are allowed.";
-  }
+ 
 }
