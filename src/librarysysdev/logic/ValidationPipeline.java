00c36e9687d98eb60d72ffc413aafff980181e58
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-02-08 23:47:08 -0600

    mod pipeline auth

--- src/librarysysdev/logic/ValidationPipeline.java
+++ src/librarysysdev/logic/ValidationPipeline.java
@@ -6,4 +6,11 @@ import java.util.List;
 public class ValidationPipeline {
   private final List<Validator<T>> validators = new ArrayList<>();
   private final List<String> ErrorList = new ArrayList<>();
+  
+  public boolean validatePipeline(Validator<T> validator) {
+    for(Validator<T> validator : validators) {
+      if(validator.validate(this))
+    }
+  }
+  
 }
